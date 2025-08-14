51aefc30bb8fe7f7e1084d1ab82a5ebc564b0a4f
akaci <akaci@pc.com>
2025-08-14 22:44:47 +0200

    refactored main

--- cmd/gomanga/main.go
+++ cmd/gomanga/main.go
@@ -1,9 +1,13 @@
 package gomanga
 
 import (
+	"context"
 	"os"
 
 	"github.com/akarakai/gomanga-tbot/pkg/logger"
+	"github.com/akarakai/gomanga-tbot/pkg/repository"
+	"github.com/akarakai/gomanga-tbot/pkg/scraper"
+	"github.com/akarakai/gomanga-tbot/pkg/telegram"
 	"github.com/joho/godotenv"
 )
 
@@ -20,5 +24,26 @@ func main() {
 		logger.Log.Panicln("TELEGRAM_API_KEY is not present in the env variables")
 	}
 
-	StartTelegramBot(telegramKey)
+	repo, err := repository.NewSqlite3Database("./database.db")
+	if err != nil {
+		logger.Log.Panicw("could not connect to the database", "err", err)
+	}
+
+	s, err := scraper.NewWeebCentralScraperDefault()
+	if err != nil {
+		logger.Log.Panicw("could not connect to the scraper", "err", err)
+	}
+
+	tg, err := telegram.NewTelegramService(
+		context.Background(),
+		telegramKey,
+		repo,
+		s,
+	)
+
+	if err != nil {
+		logger.Log.Panicw("could not create bot instance", "err", err)
+	}
+
+	tg.Start()
 }
